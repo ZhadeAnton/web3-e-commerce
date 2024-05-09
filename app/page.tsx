b1083f5fe683a690eba6fd5ee27320509d25f52f
Антон Жаде <a.zhade@podeli.ru>
2024-05-09 18:04:53 +0300

    main: install UI framework

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <main className="flex min-h-screen flex-col">Hello Web3</main>;
+  return (
+    <main className="flex min-h-screen flex-col">
+      <div>
+        <p>Hello Web3</p>
+      </div>
+      <div>
+        <button className="btn btn-primary">Primary</button>
+      </div>
+    </main>
+  );
 }
