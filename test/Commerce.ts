4259ad4595ed8fd47757595aaa56c5e0f7ab9c6b
Антон Жаде <a.zhade@podeli.ru>
2024-05-09 19:12:27 +0300

    main: update Commerce contract test blocks

--- test/Commerce.ts
+++ test/Commerce.ts
@@ -2,21 +2,21 @@ import { loadFixture } from "@nomicfoundation/hardhat-toolbox-viem/network-helpe
 import { expect } from "chai";
 import hre from "hardhat";
 
-describe("Commerce", () => {
-  async function deployCommerceFixture() {
-    const [owner, otherAccount] = await hre.viem.getWalletClients();
-    const publicClient = await hre.viem.getPublicClient();
-    const commerce = await hre.viem.deployContract("Commerce");
+async function deployCommerceFixture() {
+  const [owner, otherAccount] = await hre.viem.getWalletClients();
+  const publicClient = await hre.viem.getPublicClient();
+  const commerce = await hre.viem.deployContract("Commerce");
 
-    return {
-      commerce,
-      owner,
-      otherAccount,
-      publicClient
-    };
-  }
+  return {
+    commerce,
+    owner,
+    otherAccount,
+    publicClient
+  };
+}
 
-  it("Deploy", async () => {
+describe("Deploy", () => {
+  it("should has a name", async () => {
     const { commerce } = await loadFixture(deployCommerceFixture);
     expect(await commerce.read.name()).to.equal("E-Commerce");
   });
