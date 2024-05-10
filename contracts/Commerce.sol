a830117e3163184f5a071e0015aaec08cbebeeaf
Антон Жаде <a.zhade@podeli.ru>
2024-05-10 16:14:37 +0300

    main: finish commerce smart contract

--- contracts/Commerce.sol
+++ contracts/Commerce.sol
@@ -15,8 +15,16 @@ contract Commerce {
         uint256 stock;
     }
 
-    mapping(uint256 => Item) public items;
+    struct Order {
+        uint256 time;
+        Item item;
+    }
 
+    mapping(uint256 => Item) public items;
+    mapping(address => uint256) public orderCount;
+    mapping(address => mapping(uint256 => Order)) public orders;
+
+    event Buy(address buyer, uint256 orderId, uint256 itemId);
     event List(string name, uint256 cost, uint256 quantity);
 
     modifier onlyOwner() {
@@ -56,5 +64,25 @@ contract Commerce {
         uint256 _id
     ) public view onlyOwner returns (Item memory) {
         return items[_id];
+    }
+
+    function buy(uint256 _id) public payable {
+        Item memory item = items[_id];
+
+        require(msg.value >= item.cost);
+        require(item.stock > 0);
+
+        Order memory order = Order(block.timestamp, item);
+
+        orderCount[msg.sender]++;
+        orders[msg.sender][orderCount[msg.sender]] = order;
+
+        items[_id].stock = item.stock - 1;
+        emit Buy(msg.sender, orderCount[msg.sender], item.id);
+    }
+
+    function withdraw() public onlyOwner {
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success);
     }
 }
